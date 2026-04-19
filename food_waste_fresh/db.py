a91eac01859e1794de84a23323efe315b58e37f3
vedantmowad <vedantmowad@gmail.com>
2026-04-19 15:41:52 +0530

    Fix indentation for DB_PORT environment variable

--- food_waste_fresh/db.py
+++ food_waste_fresh/db.py
@@ -7,5 +7,5 @@ def get_connection():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306))
+        port=int(os.getenv("DB_PORT")
     )
