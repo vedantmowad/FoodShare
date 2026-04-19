373280041cd580e3391838452821ce96a99ae4e6
vedantmowad <vedantmowad@gmail.com>
2026-04-19 14:33:36 +0530

    Update db.py

--- food_waste_fresh/app/models/db.py
+++ food_waste_fresh/app/models/db.py
@@ -1,3 +1,14 @@
-from flask_mysqldb import MySQL
+import mysql.connector
+import os
+from urllib.parse import urlparse
 
-mysql = MySQL()
+def get_connection():
+    db_url = urlparse(os.getenv("DATABASE_URL"))
+
+    return mysql.connector.connect(
+        host=db_url.hostname,
+        user=db_url.username,
+        password=db_url.password,
+        database=db_url.path.lstrip("/"),
+        port=db_url.port
+    )
