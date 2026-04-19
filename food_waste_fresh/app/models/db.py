71530e77911199e8713836bc7ff8662b77772791
vedantmowad <vedantmowad@gmail.com>
2026-04-19 15:14:10 +0530

    Switch database connection from MySQL to PostgreSQL

--- food_waste_fresh/app/models/db.py
+++ food_waste_fresh/app/models/db.py
@@ -1,14 +1,19 @@
-import mysql.connector
 import os
+import psycopg2
 from urllib.parse import urlparse
 
 def get_connection():
-    db_url = urlparse(os.getenv("DATABASE_URL"))
+    db_url = os.environ.get("DATABASE_URL")
 
-    return mysql.connector.connect(
-        host=db_url.hostname,
-        user=db_url.username,
-        password=db_url.password,
-        database=db_url.path.lstrip("/"),
-        port=db_url.port
+    if not db_url:
+        raise Exception("DATABASE_URL not found")
+
+    result = urlparse(db_url)
+
+    return psycopg2.connect(
+        database=result.path[1:],
+        user=result.username,
+        password=result.password,
+        host=result.hostname,
+        port=result.port
     )
