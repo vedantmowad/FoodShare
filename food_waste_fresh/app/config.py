43b776f3dee52cd3378105c080bffad61068e0a2
vedantmowad <vedantmowad@gmail.com>
2026-04-19 15:38:29 +0530

    Add default SECRET_KEY to config
    Set default value for SECRET_KEY in config.

--- food_waste_fresh/app/config.py
+++ food_waste_fresh/app/config.py
@@ -1,10 +1,4 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
-    MYSQL_HOST = os.environ.get('MYSQL_HOST')
-    MYSQL_USER = os.environ.get('MYSQL_USER')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-    MYSQL_DB = os.environ.get('MYSQL_DB')
-    MYSQL_PORT = os.environ.get('MYSQL_PORT', 3306)
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
