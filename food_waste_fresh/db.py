61330d04ce93500584ba9ef6c4bcf28926ebc79e
vedantmowad <vedantmowad@gmail.com>
2026-04-19 15:44:06 +0530

    Fix syntax error in DB connection port assignment

--- food_waste_fresh/db.py
+++ food_waste_fresh/db.py
@@ -7,5 +7,5 @@ def get_connection():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT")
+        port=int(os.getenv("DB_PORT"))
     )
